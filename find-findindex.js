5f26e8b5c3027f7053757fe9c76f742875776a55
economicactivist <mountainscott67@yahoo.com>
2021-04-16 16:28:51 -0700

    everything but the last exercise passes test

---
+++ find-findindex.js
@@ -0,0 +1,40 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+function findUserByUsername(usersArray, username) {
+  const foundUserByUsername = usersArray.find(val => val.username === username)
+  return foundUserByUsername
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, 
+and a string. 
+The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+function removeUser(usersArray, username) {
+  const idxToDelete = usersArray.findIndex(val => val.username === username)
+  const removedObj = idxTousersArray.splice(idxToDelete, 1)
+  console.log(removedObj)
+  console.log(idxToDelete)
+  return idxToDelete !== -1 ? removedObj[0] : undefined
+}
